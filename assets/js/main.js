8331b25c68629eefc187e4584882f894bbe4a419
adibmansuri511 <adibmansuri.developer@gmail.com>
2024-01-20 19:07:33 +0530

    All updates is done.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,61 +1,89 @@
-// program to check if a number is a float or integer value
+// program to validate an email address
 
 // Get button element and add click event listener on it and assign to element variable.
 const element = document.getElementById("myBtn");
 
-element.addEventListener("click", checkNumber);
+element.addEventListener("click", validateEmail);
 
 
-function checkNumber(e) {
+
+function validateEmail(e) {
     e.preventDefault();
 
-    // Take input value from the user and convert it into a number.
-    let inputValue = document.getElementById("inputValue").value;
+    // Take input value from the user.
+    const email = document.getElementById("email").value;
 
-    console.log(`Input value is : ${inputValue}`);
-    console.log(`Input value data type is : ${typeof inputValue}`);
+    // Show the result on this #h5 id.
+    let result = document.querySelector('#h5');
 
-    // check if the passed value is a number
-    // The typeof operator is used to check the data type of the passed value.
-    // The isNaN() method checks if the passed value is a number.
+    const regexPattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    // Check the input field is not a blank.
-    if (inputValue) {
+    if (regexPattern.test(email)) {
 
-        inputValue = Number(inputValue);
-        console.log(`Input value data type is : ${typeof inputValue}`);
+        console.log(`${email} is valid email address.`);
+        // result.classList.add("input-color");
+        // result.innerHTML = `${email} is valid email address.`;
+        result.innerHTML = `<span style="color: #000;">${email}</span> is valid email address.`;
 
-
-        if (typeof inputValue === 'number' && !isNaN(inputValue)) {
-
-            // check if it is integer
-            // The Number.isInteger() method is used to check if the number is an integer value.
-            if (Number.isInteger(inputValue)) {
-
-                document.querySelector('#h5').innerHTML = `Result : ${inputValue} is integer value.`;
-
-                console.log(`${inputValue} is integer value.`);
-
-            } else {
-
-                document.querySelector('#h5').innerHTML = `Result : ${inputValue} is a float value.`;
-
-                console.log(`${inputValue} is a float value.`);
-
-            }
-
-        } else {
-
-            document.querySelector('#h5').innerHTML = `Result : ${inputValue} is not a number.`;
-            console.log(`${inputValue} is not a number.`);
-
-        }
 
     } else {
 
-        document.querySelector('#h5').innerHTML = `Please enter the value.`;
-        console.log(`Please enter the value.`);
+        console.log(`${email} is not valid email address.`);
+        result.innerHTML = `<span style="color: #000;">${email}</span> is not valid email address.`;
 
     }
 
-}
+};
+
+
+
+// function checkNumber(e) {
+//     e.preventDefault();
+
+//     // Take input value from the user and convert it into a number.
+//     // const email = document.getElementById("email").value;
+
+//     console.log(`Input value is : ${email}`);
+
+//     // check if the passed value is a number
+//     // The typeof operator is used to check the data type of the passed value.
+//     // The isNaN() method checks if the passed value is a number.
+
+//     // Check the input field is not a blank.
+//     if (email) {
+
+
+
+//         if (typeof email === 'number' && !isNaN(email)) {
+
+//             // check if it is integer
+//             // The Number.isInteger() method is used to check if the number is an integer value.
+//             if (Number.isInteger(email)) {
+
+//                 document.querySelector('#h5').innerHTML = `Result : ${email} is integer value.`;
+
+//                 console.log(`${email} is integer value.`);
+
+//             } else {
+
+//                 document.querySelector('#h5').innerHTML = `Result : ${email} is a float value.`;
+
+//                 console.log(`${email} is a float value.`);
+
+//             }
+
+//         } else {
+
+//             document.querySelector('#h5').innerHTML = `Result : ${email} is not a number.`;
+//             console.log(`${email} is not a number.`);
+
+//         }
+
+//     } else {
+
+//         document.querySelector('#h5').innerHTML = `Please enter the value.`;
+//         console.log(`Please enter the value.`);
+
+//     }
+
+// }
